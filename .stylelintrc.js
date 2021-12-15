1259375ff8101075213f85d37aa589ea6ec94b83
Andrey Voronin <AVor0n@yandex.ru>
2021-12-15 10:14:44 +0500

    init: environment setup

---
+++ .stylelintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+  extends: [
+    'stylelint-config-standard',
+    'stylelint-config-prettier',
+    'stylelint-config-rational-order',
+  ],
+  plugins: [
+    'stylelint-order',
+    'stylelint-config-rational-order/plugin',
+    'stylelint-no-unsupported-browser-features',
+  ],
+  rules: {
+    'order/properties-order': [],
+    'plugin/rational-order': [
+      true,
+      {
+        'empty-line-between-groups': false,
+      },
+    ],
+    'plugin/no-unsupported-browser-features': [
+      true,
+      {
+        severity: 'warning',
+      },
+    ],
+    'declaration-empty-line-before': null, //конфликтует с rational-order
+  },
+  ignoreFiles: ['**/reset.css'],
+};
